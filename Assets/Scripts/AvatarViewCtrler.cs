c7de97d91f0eaab242828fa7d5c253f1a34226d0
seojoonwon <seojoonwon@naver.com>
2016-10-20 17:50:00 +0900

    초기 아바타 설정화면 및 커스터마이징 화면 NGUI로 재구성.

---
+++ Assets/Scripts/AvatarViewCtrler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class AvatarViewCtrler : MonoBehaviour {
+    public GameObject 
+        selectPanel,
+        menuPanel;
+
+    public void OnSelectPanel() {
+        selectPanel.SetActive(true);
+    }
+
+    public void OffSelectPanel() {
+        selectPanel.SetActive(false);
+    }
+
+    public void OffMenuPanel() {
+        menuPanel.SetActive(false);
+    }
+}
