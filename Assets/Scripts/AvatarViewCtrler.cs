fbe78f6c1c12e4b30125fd2f6d127a478e187f99
seojoonwon <seojoonwon@naver.com>
2016-10-21 13:40:10 +0900

    줌인 기능 수정. 확인 버튼 기능 비활성화. 뒤로가기 버튼 기능추가.  남성 기본적으로 선택되어있는 상태로 변경.

--- Assets/Scripts/AvatarViewCtrler.cs
+++ Assets/Scripts/AvatarViewCtrler.cs
@@ -7,6 +7,20 @@ public class AvatarViewCtrler : MonoBehaviour {
         selectPanel,
         menuPanel;
 
+    public Transform[] avatarElementPos;
+    private Vector3 cameraOriginPos;
+    private float 
+        startTime,
+        speed = 3f;
+
+    private bool canZoom = true;
+
+    void Start() {
+        cameraOriginPos = Camera.main.transform.position;
+        startTime = Time.time;
+        Debug.Log(startTime);
+    }
+
     public void OnSelectPanel() {
         selectPanel.SetActive(true);
     }
@@ -17,5 +31,41 @@ public class AvatarViewCtrler : MonoBehaviour {
 
     public void OffMenuPanel() {
         menuPanel.SetActive(false);
+    }
+
+    public void moveToHair() {
+        StartCoroutine("move", avatarElementPos[0].transform.position);
+    }
+
+    public void moveToHead() {
+        StartCoroutine("move", avatarElementPos[1].transform.position);
+    }
+
+    public void moveToBody() {
+        StartCoroutine("move", avatarElementPos[2].transform.position);
+        Debug.Log("endPos : " + avatarElementPos[2].transform.position);
+    }
+
+    public void returnToOrigin() {
+        StopCoroutine("move");
+        Camera.main.transform.position = cameraOriginPos;
+    }
+
+    IEnumerator move(Vector3 endPos) {
+        float journeyLength = Vector3.Distance(cameraOriginPos, endPos);
+        endPos.z -= 1f;
+        float _time = 0;
+        Debug.Log("endPos : " + endPos);
+        while (canZoom) {
+            _time += Time.deltaTime;
+            float disCovered = (_time - startTime) * speed;
+            float fracJourney = disCovered / journeyLength;
+            Camera.main.transform.position = Vector3.Lerp(cameraOriginPos, endPos, fracJourney);
+            if (Camera.main.transform.position.z >= endPos.z) {
+                canZoom = false;
+            }
+            yield return null;
+        }
+        canZoom = true;
     }
 }
