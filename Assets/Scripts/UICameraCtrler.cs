da05e8e1461232b872feac82a4790d60700ba38e
seojoonwon <seojoonwon@naver.com>
2016-10-27 16:18:52 +0900

    라이딩 결과 scene 추가

---
+++ Assets/Scripts/UICameraCtrler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UICameraCtrler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Camera _camera = gameObject.GetComponent<Camera>();
+        _camera.orthographicSize = (Screen.height * 720f / 1280f) / Screen.width;
+        Debug.Log(_camera.orthographicSize);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
