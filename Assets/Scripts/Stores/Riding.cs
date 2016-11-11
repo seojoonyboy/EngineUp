f5f0f6aa310efa774b15e76436ba9251f6643f92
박선우 <shabangrrr@fbl.kr>
2016-11-11 16:26:16 +0900

    GameManager에 StringBuilder 클래스 추가, Riding에서 gps로그 기록하는 기능 추가

--- Assets/Scripts/Stores/Riding.cs
+++ Assets/Scripts/Stores/Riding.cs
@@ -1,6 +1,8 @@
 using Flux;
 using UnityEngine;
 using System;
+using System.IO;
+using System.Text;
 
 public class Riding : Store<Actions>{
     LocationInfo? _preLocation = null;
@@ -10,13 +12,14 @@ public class Riding : Store<Actions>{
     public float avgSpeed;
     public TimeSpan totalTime;
 
-
+    private string dataFilePath;
     private const float EARTH_RADIUS = 6371;
 
     public Riding(Dispatcher<Actions> _dispatcher):base(_dispatcher){}
 
     private void _gpsOperation(LocationInfo loc){
         totalTime = DateTime.Now - startTime;
+        _writeData(loc);
         if(_preLocation == null){
             _preLocation = loc;
             return;
@@ -50,11 +53,43 @@ public class Riding : Store<Actions>{
 
         return EARTH_RADIUS * c;
     }
+
+    void _writeData(LocationInfo loc){
+        System.Text.StringBuilder _sb = GameManager.Instance.sb;
+        _sb.Remove(0,_sb.Length);
+        _sb.Append(loc.altitude).Append("|")
+            .Append(loc.latitude).Append("|")
+            .Append(loc.longitude).Append("|")
+            .Append(loc.timestamp).Append("|")
+            .Append(loc.horizontalAccuracy).Append("|")
+            .Append(loc.verticalAccuracy).Append("\n");
+        File.AppendAllText(dataFilePath, _sb.ToString(), Encoding.UTF8);
+    }
+
+    void _dataFileInit(DateTime now){
+        System.Text.StringBuilder _sb = GameManager.Instance.sb;
+        _sb.Remove(0,_sb.Length);
+        _sb.Append(Application.persistentDataPath).Append("/");
+        string d = now.ToString("s");
+        int count = 0;
+        d = d.Replace("T","")
+            .Replace("-","")
+            .Replace(":","");
+        _sb.Append(d).Append(count);
+        while(File.Exists(_sb.ToString())){
+            count++;
+            _sb.Remove(_sb.Length-1,1);
+            _sb.Append(count);
+        }
+        dataFilePath = _sb.ToString();
+        Debug.Log(dataFilePath);
+    }
     protected override void _onDispatch(Actions action){
         switch(action.type){
         case ActionTypes.RIDING_START:
             startTime = DateTime.Now;
             totalDist = 0;
+            _dataFileInit(startTime);
             break;
         case ActionTypes.GET_GPS_DATA:
             GetGPSDataAction _act = action as GetGPSDataAction;
--- Assets/Scripts/View/GameManager.cs
+++ Assets/Scripts/View/GameManager.cs
@@ -1,7 +1,9 @@
 ﻿using Flux;
+using System.Text;
 public class GameManager : Singleton<GameManager> {
     protected GameManager(){}
     public Dispatcher<Actions> gameDispatcher = new Dispatcher<Actions>();
+    public StringBuilder sb = new StringBuilder();
 
     public User userStore;
 
