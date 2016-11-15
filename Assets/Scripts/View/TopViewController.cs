3d435558f2ecadced4643f9021a498302e6148c2
seojoonwon <seojoonwon@naver.com>
2016-11-15 18:45:32 +0900

    Main 화면 디자인 초안 적용

--- Assets/Scripts/View/TopViewController.cs
+++ Assets/Scripts/View/TopViewController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class TopViewController : MonoBehaviour {
     void Start() {
-        UILabel nickNameLabel = gameObject.transform.Find("InfoPanel/Label").GetComponent<UILabel>();
+        UILabel nickNameLabel = gameObject.transform.Find("InfoPanel/NickName").GetComponent<UILabel>();
         //Debug.Log(GameManager.Instance.userStore.nickName);
         nickNameLabel.text = GameManager.Instance.userStore.nickName;
     }
