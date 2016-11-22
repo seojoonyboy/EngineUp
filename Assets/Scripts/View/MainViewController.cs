a766bdb3fdec5db62c77b6226c47b119420a08c6
seojoonwon <seojoonwon@naver.com>
2016-11-22 16:34:42 +0900

    Main 화면 버튼, Parameter를 이용한 이벤트 등록 방식으로 수정.

--- Assets/Scripts/View/MainViewController.cs
+++ Assets/Scripts/View/MainViewController.cs
@@ -1,26 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum MAIN_BUTTON {
+    HOME,
+    ITEM,
+    AVATAR,
+    HISTORY,
+    SHOP,
+    RIDING,
+    BATTLE
+};
+
 public class MainViewController : MonoBehaviour {
     public GameObject
         ridingPanel,
         uploadPanel;
 
-    public void onRidingPanel() {
-        Actions act = ActionCreator.createAction(ActionTypes.RIDING_START);
-        GameManager.Instance.gameDispatcher.dispatch(act);
-        ridingPanel.SetActive(true);
+    public UIButton[] mainBtns;
+    
+    void Start() {
+        addMainBtnEvent(0,MAIN_BUTTON.RIDING);
     }
 
-    public void offRidingPanel() {
-        ridingPanel.SetActive(false);
+    public void onMainBtnEvent(MAIN_BUTTON type) {
+        switch(type) {
+            case MAIN_BUTTON.RIDING:
+            Actions act = ActionCreator.createAction(ActionTypes.RIDING_START);
+            GameManager.Instance.gameDispatcher.dispatch(act);
+            ridingPanel.SetActive(true);
+            break;
+        }
     }
 
     public void onUploadPanel() {
+        ridingPanel.SetActive(false);
         uploadPanel.SetActive(true);
     }
 
-    public void offUploadPanel() {
-        uploadPanel.SetActive(false);
+    private void addMainBtnEvent(int index, MAIN_BUTTON type) {
+        EventDelegate eventBtn = new EventDelegate(gameObject.GetComponent<MainViewController>(),"onMainBtnEvent");
+
+        eventBtn.parameters[0].value = type;
+
+        EventDelegate.Set(mainBtns[index].onClick,eventBtn);
+        EventDelegate.Add(mainBtns[index].GetComponent<UIButton>().onClick,eventBtn);
     }
 }
