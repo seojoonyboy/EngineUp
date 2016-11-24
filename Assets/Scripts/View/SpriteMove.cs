0bac3bc4817a44787c7605167cadb3260ec29eec
seojoonwon <seojoonwon@naver.com>
2016-11-24 15:30:02 +0900

    아바타 몸통 각지는 현상 수정.(Import 설정 변경)

---
+++ Assets/Scripts/View/SpriteMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteMove : MonoBehaviour {
+    public GameObject[] sprites;
+    public Transform[] targets;
+
+    void Start() {
+        StartCoroutine("move");
+    }
+
+    IEnumerator move() {
+        while(true) {
+            foreach(GameObject sprite in sprites) {
+                Vector2 newPos;
+                if(sprite.transform.position.x <= targets[0].transform.position.x) {
+                    newPos = new Vector2(targets[1].transform.position.x, sprite.transform.position.y);
+                }
+                else {
+                    newPos = new Vector2(sprite.transform.position.x - 0.1f, sprite.transform.position.y);                    
+                }
+                sprite.transform.position = newPos;
+            }
+            yield return new WaitForSeconds(0.5f);
+            
+        }        
+    }
+}
