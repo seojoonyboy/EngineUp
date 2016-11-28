65e42361810bc2f47ab09eee367eb648488e6cd2
seojoonwon <seojoonwon@naver.com>
2016-11-28 13:39:38 +0900

    메인 버튼 이벤트 등록 관련 Component 기존 Main View Controller에서 분리

---
+++ Assets/Scripts/MainButtonListener.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum MAIN_BUTTON {
+    HOME,
+    ITEM,
+    AVATAR,
+    HISTORY,
+    SHOP,
+    RIDING,
+    BATTLE
+};
+
+public class MainButtonListener : MonoBehaviour {
+    public UIButton[] mainBtns;
+    void Start() {
+        addMainBtnEvent(0,MAIN_BUTTON.RIDING);
+    }
+
+    private void addMainBtnEvent(int index,MAIN_BUTTON type) {
+        EventDelegate eventBtn = new EventDelegate(gameObject.GetComponent<MainViewController>(),"onMainBtnEvent");
+
+        eventBtn.parameters[0].value = type;
+
+        EventDelegate.Set(mainBtns[index].onClick,eventBtn);
+        EventDelegate.Add(mainBtns[index].GetComponent<UIButton>().onClick,eventBtn);
+    }
+}
--- Assets/Scripts/View/MainViewController.cs
+++ Assets/Scripts/View/MainViewController.cs
@@ -1,27 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public enum MAIN_BUTTON {
-    HOME,
-    ITEM,
-    AVATAR,
-    HISTORY,
-    SHOP,
-    RIDING,
-    BATTLE
-};
-
 public class MainViewController : MonoBehaviour {
     public GameObject
         ridingPanel,
         uploadPanel,
         avatar;
-
-    public UIButton[] mainBtns;
-    
-    void Start() {
-        addMainBtnEvent(0,MAIN_BUTTON.RIDING);
-    }
 
     public void onMainBtnEvent(MAIN_BUTTON type) {
         switch(type) {
@@ -42,14 +26,5 @@ public class MainViewController : MonoBehaviour {
     public void offUploadPanel() {
         avatar.SetActive(true);
         uploadPanel.SetActive(false);
-    }
-
-    private void addMainBtnEvent(int index, MAIN_BUTTON type) {
-        EventDelegate eventBtn = new EventDelegate(gameObject.GetComponent<MainViewController>(),"onMainBtnEvent");
-
-        eventBtn.parameters[0].value = type;
-
-        EventDelegate.Set(mainBtns[index].onClick,eventBtn);
-        EventDelegate.Add(mainBtns[index].GetComponent<UIButton>().onClick,eventBtn);
     }
 }
