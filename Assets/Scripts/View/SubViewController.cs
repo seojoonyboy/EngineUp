553f4fdcb100e0cfe703962ca2105311a211e178
seojoonwon <seojoonwon@naver.com>
2016-11-28 15:24:41 +0900

    Result 등 Main View 이외의 View를 처리할 Sub Controller 추가

---
+++ Assets/Scripts/View/SubViewController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubViewController : MonoBehaviour {
+    public GameObject
+        resultPanel;
+
+    void Start() {
+
+    }
+
+    public void onResultPanel() {
+        resultPanel.SetActive(true);
+    }
+}
