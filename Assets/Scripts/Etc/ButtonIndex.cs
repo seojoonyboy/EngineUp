0bba18e5ee34ad305c204a6d3c65152c4ab29526
seojoonwon <seojoonwon@naver.com>
2016-12-11 15:10:40 +0900

    메인 버튼 인덱스를 통한 처리로 수정.

---
+++ Assets/Scripts/Etc/ButtonIndex.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonIndex : MonoBehaviour {
+    public int index = -1;
+    public GameObject viewController;
+
+    public void OnClick() {
+        viewController.GetComponent<AvatarViewController>().CustomBtnClick(index);
+    }
+}
