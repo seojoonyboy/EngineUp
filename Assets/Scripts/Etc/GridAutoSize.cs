b91e36396e240ba49beb9a19b14207eebcd4860b
seojoonwon <seojoonwon@naver.com>
2016-12-21 15:11:19 +0900

    아바타 자전거 프레임 Material 적용. StartLoading 화면 레이아웃 전면 수정. 화면 해상도에 따라 Select Panel의 Scroll View 내부의 Grid Size 및 Item 크기 변동되도록 추가 구현.

---
+++ Assets/Scripts/Etc/GridAutoSize.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridAutoSize : MonoBehaviour {
+    UIGrid mGrid;
+    UIPanel parentPanel;
+    Component[] uiSprites;
+
+    void OnEnable() {
+        mGrid = GetComponent<UIGrid>();
+        parentPanel = transform.parent.GetComponent<UIPanel>();
+        uiSprites = GetComponentsInChildren(typeof(UISprite));
+        foreach(UISprite sprite in uiSprites) {
+            sprite.SetDimensions((int)parentPanel.height, (int)parentPanel.height);
+        }
+        mGrid.cellWidth = GetComponentInChildren<UISprite>().width * 1.5f;
+    }
+}
