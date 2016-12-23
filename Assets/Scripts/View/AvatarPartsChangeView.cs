e9533be5748e93fb715cdc5b45da5759a594241c
seojoonwon <seojoonwon@naver.com>
2016-12-23 17:15:43 +0900

    아바타 커스터마이징 화면에서 파트를 선택시 Select Panel 에 뿌려줄 Item을 결정하기 위한 Code 추가.

---
+++ Assets/Scripts/View/AvatarPartsChangeView.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AvatarPartsChangeView : MonoBehaviour {
+    GameManager gameManager;
+    User userStore;
+
+    void Start() {
+        gameManager = GameManager.Instance;
+        userStore = gameManager.userStore;
+    }
+
+    void OnEnable() {
+        SetItemList();
+    }
+
+    void SetItemList() {
+
+    }
+}
