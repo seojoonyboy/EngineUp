5d4e06855a69bae0a05ad2a3c7ec1bf90b4ffc44
seojoonwon <seojoonwon@naver.com>
2016-12-23 13:35:55 +0900

    NetworkManager 이전 Server 요청 함수 제거.

--- Assets/Scripts/Managers/NetworkManager.cs
+++ Assets/Scripts/Managers/NetworkManager.cs
@@ -36,36 +36,6 @@ public class NetworkManager : Singleton<NetworkManager> {
         yield return www.Send();
         callback(new HttpResponse(www));
     }
-
-    public void _send(string url,string nickname,string data) {
-        StartCoroutine(_sendToServer(url,nickname,data));
-    }
-
-    IEnumerator _sendToServer(string url,string nickname,string data) {
-        WWWForm form = new WWWForm();
-        //Debug.Log(ridingStore.resultData.ToString());
-        form.AddField("name",nickname);
-        form.AddField("location_name","ChunCheon");
-        form.AddField("location_desc","Romantic");
-        form.AddField("raw_data",data);
-
-        UnityWebRequest www = UnityWebRequest.Post(url,form);
-        yield return www.Send();
-
-        ActionTypes type = ActionTypes.POST_FAIL;
-        if(www.isError) {
-            type = ActionTypes.POST_FAIL;
-            Debug.Log(www.error);
-        }
-
-        else {
-            type = ActionTypes.POST_SUCCESS;
-            Debug.Log("FORM UPLOAD COMPLETE");
-        }
-
-        Actions action = ActionCreator.createAction(type);
-        GameManager.Instance.gameDispatcher.dispatch(action);
-    }
 }
 
 public class HttpResponse {
