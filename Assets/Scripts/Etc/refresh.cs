d221a5486e7c4b15582ca870f916bcc0e7476676
seojoonwon <seojoonwon@naver.com>
2016-12-28 14:24:48 +0900

    StartLoading Scene SelectPanel 비활성화. Main Scene Community Panel의 FeedPanel에 Scroll을 상단으로 일정이상 당기고 있으면 새로고침이 되는 기능 구현을 위한 스크립트 추가(테스트용)

---
+++ Assets/Scripts/Etc/refresh.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class refresh : MonoBehaviour {
+    UIPanel panel;
+    UIScrollView scrollView;
+    float startTime, time = 0;
+	// Use this for initialization
+	void Start () {
+        panel = GetComponent<UIPanel>();
+        scrollView = GetComponent<UIScrollView>();
+        scrollView.onDragStarted += OnDragStart;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (scrollView.isDragging) {
+            if(panel.clipOffset.y >= 300f) {
+                time = Time.time - startTime;
+                //Debug.Log(time);
+                if(time >= 2) {
+                    Refresh();
+                }
+            }
+        }
+	}
+
+    void OnDragStart() {
+        startTime = Time.time;
+    }
+
+    //act like mobile pull display top and refresh
+    void Refresh() {
+        Debug.Log("Refresh");
+    }
+}
