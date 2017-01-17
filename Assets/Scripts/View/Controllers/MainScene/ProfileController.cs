a7d7b826ebe3ee9bf6abf5b0a475aed2baf43a02
seojoonwon <seojoonwon@naver.com>
2017-01-17 14:21:10 +0900

    메인화면 Top Panel 디자인 수정. Profile 전용 패널 및 Scroll 기능 추가

---
+++ Assets/Scripts/View/Controllers/MainScene/ProfileController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProfileController : MonoBehaviour {
+    Animation anim;
+    public GameObject[] arrows;
+
+    public bool
+        isOver,
+        isDown;
+    void Start() {
+        anim = GetComponent<Animation>();
+        isOver = true;
+        isDown = false;
+    }
+
+    public void Scroll() {
+        if (isOver) {
+            isOver = false;
+            if (isDown) {
+                anim.Play("Up");
+            }
+            else {
+                anim.Play("Down");
+            }
+        }
+    }
+
+    public void scrollDownDone() {
+        isOver = true;
+        isDown = true;
+        arrows[0].transform.localRotation = new Quaternion(0, 180, 0, 1);
+        arrows[1].transform.localRotation = new Quaternion(0, 180, 0, 1);
+    }
+
+    public void scrollUpDone() {
+        isOver = true;
+        isDown = false;
+    }
+}
