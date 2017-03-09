93e19697ead3b796f21a4ad78a536919d06bac50
seojoonwon <seojoonwon@naver.com>
2017-03-09 18:34:56 +0900

    Mapline cnt 변수 초기화 위치 수정.

--- Assets/Scripts/Etc/MapLine.cs
+++ Assets/Scripts/Etc/MapLine.cs
@@ -4,14 +4,14 @@ using System.Collections.Generic;
 public class MapLine : MonoBehaviour {
     public void drawLine(float[] lat, float[] lon) {
         List<Vector2> list = new List<Vector2>();
-
-        for(int i=0; i<lat.Length-1;i++) {
-            int cnt = 0;
+        int cnt = 0;
+        for (int i=0; i<lat.Length; i++) {
             if(cnt == 2) {
                 float midLon = Mathf.Abs(lon[i - 2] + lon[i]) / 2f;
                 float midLat = Mathf.Abs(lat[i - 2] + lat[i]) / 2f;
 
                 Vector2 loc = new Vector2(midLon, midLat);
+                Debug.Log("Mid Lon : " + midLon);
                 list.Add(loc);
                 cnt = 0;
             }
