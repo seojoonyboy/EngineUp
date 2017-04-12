0a4adce69b06368089509a39b2d741ca045707be
seojoonwon <seojoonwon@naver.com>
2017-04-12 15:47:11 +0900

    차고지 Toggle Group 기존, NGUI Toggle Group 사용하던 것 자체 Toggle 제어 Component 제작하여 대체 (기존 NGUI의 경우 Toggle Group을 나가도 이전에 마지막으로 활성화한 Panel이 그대로 활성화되어있는 문제가 있었음)

---
+++ Assets/Scripts/Etc/fbl_ToggleObjects.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fbl_ToggleObjects : MonoBehaviour {
+    public GameObject[] active;
+    public GameObject[] deactive;
+
+    public bool isFirstActive = false;
+}
