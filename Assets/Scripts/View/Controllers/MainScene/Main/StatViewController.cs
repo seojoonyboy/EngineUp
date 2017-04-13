4c636b3fc92fac8143dd2342a8382cb0d609b536
seojoonwon <seojoonwon@naver.com>
2017-04-13 14:24:44 +0900

    내 정보 화면에 내 Stat(힘, 스피드, 지구력, 회복력), 닉네임, 등급 표시 추가.

--- Assets/Scripts/View/Controllers/MainScene/Main/StatViewController.cs
+++ Assets/Scripts/View/Controllers/MainScene/Main/StatViewController.cs
@@ -5,11 +5,66 @@ public class StatViewController : MonoBehaviour {
     public Riding ridingStore;
     public User userStore;
 
+    public UILabel[] 
+        stats,
+        profiles,
+        totalRidings,
+        monthlyRidings;
+
+    private GameManager gm;
+
+    void Awake() {
+        gm = GameManager.Instance;
+    }
+
+    void OnEnable() {
+        initialize();
+
+    }
+
     public void onUserListener() {
         nickNameLabel.text = userStore.nickName;
+        initialize();
     }
 
     void Start() {
         nickNameLabel.text = GameManager.Instance.userStore.nickName;
+    }
+
+    public void offPanel() {
+        gameObject.SetActive(false);
+    }
+
+    private void initialize() {
+        setProfile();
+        setStats();
+        setTotalRidingRecord();
+        setMonthlyRidingRecord();
+    }
+
+    private void setProfile() {
+        UserData data = userStore.myData;
+        profiles[0].text = data.nickName;
+
+        status statData = data.status;
+        profiles[1].text = statData.rank.ToString();
+        //그룹
+
+        stats[0].text = statData.strength.ToString();
+        stats[1].text = statData.speed.ToString();
+        stats[2].text = statData.endurance.ToString();
+        stats[3].text = statData.regeneration.ToString();
+    }
+
+    private void setStats() {
+        
+    }
+
+    private void setTotalRidingRecord() {
+        
+    }
+
+    private void setMonthlyRidingRecord() {
+        
     }
 }
