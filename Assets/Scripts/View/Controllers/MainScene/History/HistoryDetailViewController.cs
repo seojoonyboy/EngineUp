1aa48cdf276ac6fd9c7e42e65fb197014bcbf3d3
seojoonwon <seojoonwon@naver.com>
2017-04-14 14:03:27 +0900

    dummy data를 이용한 기록실 구성. 기록 상세보기에서 Map 사용간 Collider 제어를 위한 HistoryViewController 추가. 마이홈 화면 디자인 적용

---
+++ Assets/Scripts/View/Controllers/MainScene/History/HistoryDetailViewController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HistoryDetailViewController : MonoBehaviour {
+    public BoxCollider[] collider;
+    public GameObject map;
+    private Vector2 preMapScale;
+
+    //지도 사용을 위해 다른 collider를 꺼준다.
+    void OnEnable() {
+        foreach(Collider col in collider) {
+            col.enabled = false;
+        }
+        setMap();
+        map.SetActive(true);
+    }
+
+    void OnDisable() {
+        foreach(Collider col in collider) {
+            col.enabled = true;
+        }
+        map.GetComponent<OnlineMaps>().tilesetSize = preMapScale;
+    }
+
+    void setMap() {
+        preMapScale = map.GetComponent<OnlineMaps>().tilesetSize;
+        map.GetComponent<OnlineMaps>().tilesetSize = new Vector2(540, 600);
+    }
+
+}
--- Assets/Scripts/View/Controllers/MainScene/History/HistoryViewController.cs
+++ Assets/Scripts/View/Controllers/MainScene/History/HistoryViewController.cs
@@ -3,6 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HistoryViewController : MonoBehaviour {
+    public GameObject 
+        container,
+        innerContainer;
+
+    public GameObject[] items;
+    public UIScrollView scrollView;
+
+    public int containerHeight = 400;
+
+    void OnEnable() {
+        makeList();
+    }
+
+    public void makeList() {
+        items = new GameObject[5];
+        for(int i = 0; i < 5; i++) {
+            items[i] = Instantiate(container);
+
+            items[i].transform.SetParent(scrollView.transform);
+            items[i].transform.localPosition = Vector3.zero;
+            items[i].transform.localScale = Vector3.one;
+
+            int childCnt = 0;
+            for(int j = 0; j < i; j++) {
+                GameObject item = Instantiate(innerContainer);
+
+                UIGrid grid = items[i].transform.Find("Grid").GetComponent<UIGrid>();
+                item.transform.SetParent(grid.transform);
+
+                item.transform.localPosition = Vector3.zero;
+                item.transform.localScale = Vector3.one;
+
+                childCnt++;
+            }
+
+            items[i].GetComponent<UIWidget>().ResetAnchors();
+
+            if (i != 0) {
+                UIAnchor anchor = items[i].AddComponent<UIAnchor>();
+                anchor.runOnlyOnce = false;
+                anchor.container = items[i - 1].transform.Find("Grid").gameObject;
+                anchor.side = UIAnchor.Side.Bottom;
+                anchor.pixelOffset = new Vector2(0, -400.0f);
+            }
+        }
+    }
+
     public void offPanel() {
         gameObject.SetActive(false);
     }
