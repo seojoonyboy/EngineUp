2dbc8e4d923e77792c24827af6b4fbebaea36358
seojoonwon <seojoonwon@naver.com>
2017-05-24 17:27:11 +0900

    사운드 관리를 위한 SoundManager 추가.

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+    public GameObject[] 
+        effectSound,
+        music;
+
+    public void playEffect(int index) {
+        effectSound[index].GetComponent<AudioSource>().Play();
+    }
+
+    public void playMusic() {
+        
+    }
+}
