60b026fd781308179639b1ee4dc19ed5788a0d74
seojoonwon <seojoonwon@naver.com>
2017-05-29 18:17:37 +0900

    내정보 생년월일 입력을 위한 DatePicker용 Canvas 누락된 것 다시 추가. 튜토리얼 일부 추가.

--- Assets/Scripts/Managers/TutotrialManager.cs
+++ Assets/Scripts/Managers/TutotrialManager.cs
@@ -3,43 +3,105 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TutotrialManager : MonoBehaviour {
-    public GameObject[] effects;
+    public GameObject[] 
+        effects,
+        buttons;
     public UILabel
         header, 
         context;
     public GameObject
+        PARTNER,
         NPC,
         myCharacter,
-        talkContainer;
-
+        talkContainer,
+        container;
+    public Riding_VC ridingController;
     Tutorial[] text;
     int count = 0;
+    bool isFirstClick = false;
+    TypewriterEffect typingEffect;
     void Start() {
         TextAsset file = (TextAsset)Resources.Load("TutorialContext");
         text = JsonHelper.getJsonArray<Tutorial>(file.text);
-        nextPage();
+        typingEffect = context.GetComponent<TypewriterEffect>();
+        EventDelegate.Add(typingEffect.onFinished, typingFinished);
+        setFirstState();
     }
 
-    public void nextPage() {
-        if(count > text.Length - 1) { return; }
-        effectInit();
-        addEffect(count);
+    void OnDisable() {
+        count = 0;
+    }
+
+    void setFirstState() {
         header.text = text[count].id.ToString();
         context.text = text[count].context;
         count++;
     }
 
+    public void nextPage() {
+        if(count > text.Length - 1) { return; }
+        if (!isFirstClick) {
+            isFirstClick = true;
+            typingEffect.Finish();
+            return;
+        }
+        effectInit();
+        addEffect(count);
+        header.text = text[count].id.ToString();
+        context.text = text[count].context;
+        count++;
+        typingEffect.ResetToBeginning();
+    }
+
+    void typingFinished() {
+        isFirstClick = true;
+    }
+
     void addEffect(int count) {
         switch(count) {
-            case 1:
+            case 3:
+            case 5:
+                PARTNER.SetActive(true);
+                break;
+            case 7:
+                shakeEffect();
+                effects[0].SetActive(true);
+                break;
+            case 8:
                 NPC.SetActive(true);
+                effects[0].SetActive(false);
+                break;
+            case 11:
+                StartCoroutine(activateButton(0));
+                talkContainer.transform.Find("Background").GetComponent<BoxCollider>().enabled = false;
+                break;
+            case 12:
+                buttons[0].SetActive(false);
+                ridingController.gameObject.SetActive(true);
+                talkContainer.transform.Find("Background").GetComponent<BoxCollider>().enabled = true;
+                break;
+            case 13:
+                talkContainer.SetActive(false);
+                StartCoroutine(activateButton(1));
                 break;
         }
     }
 
     void effectInit() {
         NPC.SetActive(false);
+        PARTNER.SetActive(false);
         myCharacter.SetActive(false);
+        isFirstClick = false;
+    }
+
+    void shakeEffect() {
+        Debug.Log("Shaking");
+        container.GetComponent<Animator>().Play("Shaking");
+    }
+
+    IEnumerator activateButton(int index) {
+        yield return new WaitForSeconds(1.0f);
+        buttons[index].SetActive(true);
     }
 }
 
